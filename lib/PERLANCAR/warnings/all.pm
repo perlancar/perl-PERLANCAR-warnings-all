85c88d9e0fe4fa51b01b523ccb735197065b9946
perlancar (@pc-office) <perlancar@gmail.com>
2016-03-23 20:10:29 +0700

    Release v0.01

--- lib/PERLANCAR/warnings/all.pm
+++ lib/PERLANCAR/warnings/all.pm
@@ -1,7 +1,7 @@
 package PERLANCAR::warnings::all;
 
-our $BITS1; # PRECOMPUTE: require warnings; warnings->import("all"); ${^WARNING_BITS}
-our $BITS2; # PRECOMPUTE: require warnings; warnings->unimport;      ${^WARNING_BITS}
+our $BITS1 = do { require warnings; warnings->import("all"); ${^WARNING_BITS} }; # PRECOMPUTE
+our $BITS2 = do { require warnings; warnings->unimport;      ${^WARNING_BITS} }; # PRECOMPUTE
 
 sub import {
     ${^WARNING_BITS} = $BITS1;
@@ -12,10 +12,21 @@ sub unimport {
 }
 
 1;
-# ABSTRACT: Turn on warnings (all)
+# ABSTRACT: Turn on warnings (all) with less code
+
+=head1 SYNOPSIS
+
+ use PERLANCAR::warnings::all;
+
+is equivalent to:
+
+ use warnings 'all';
+
+but with less code and startup overhead.
+
 
 =head1 DESCRIPTION
 
-Just an experimental module.
+Just an experimental module (probably silly).
 
 Some notes: the bits depend on the perl used when building the dist.
